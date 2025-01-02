eacc3e0f74da7a972131aaf7635ef6658f0b015e
Сулейман Хасанов <macbook@MacBook-Air.local>
2025-01-02 15:29:27 +0500

    Added first endpoint for testing backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,18 @@ import express from 'express'
 
 const expressApp = express()
 
+const ideas = [
+  { nick: 'cool-idea-1', id: 1, title: 'Learn TypeScript' },
+  { nick: 'cool-idea-2', id: 2, title: 'Build a full-stack app' },
+  { nick: 'cool-idea-3', id: 3, title: 'Learn GraphQL' },
+]
+
 expressApp.get('/ping', (req, res) => {
   res.send('pong')
+})
+
+expressApp.get('/ideas', (req, res) => {
+  res.send(ideas)
 })
 
 expressApp.listen(3000, () => {
